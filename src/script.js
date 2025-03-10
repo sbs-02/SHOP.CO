d9f637b931437da2ab8f49436fd46012be7c5ba6
sbs-02 <dangolsaksham8@gmail.com>
2025-03-10 17:11:21 +0545

    initial commit

---
+++ src/script.js
@@ -0,0 +1,8 @@
+const ad = document.getElementById("ad");
+const cross = document.getElementById("cross");
+
+cross.addEventListener("click", function () {
+  ad.classList.add("hidden");
+});
+
+/* <div class="hidden" id="ad"></div> */
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      xxs: "320px",
+
+      xs: "375px",
+
+      sm: "425px",
+
+      md: "768px",
+
+      lg: "1024px",
+
+      xl: "1280px",
+
+      "2xl": "1440px",
+
+      "3xl": "1600px",
+
+      "4xl": "1800px",
+
+      "5xl": "2000px",
+
+      "6xl": "2200px",
+
+      "7xl": "2400px",
+
+      "8xl": "2560px",
+    },
+  },
+  plugins: [],
+};
