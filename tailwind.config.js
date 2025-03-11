ebb0b326eb12e6f6ddb63b67d83b9a233315c4c6
sbs-02 <dangolsaksham8@gmail.com>
2025-03-11 17:15:03 +0545

    add prettier and tailwindcss plugin configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,14 +21,6 @@ module.exports = {
       "3xl": "1600px",
 
       "4xl": "1800px",
-
-      "5xl": "2000px",
-
-      "6xl": "2200px",
-
-      "7xl": "2400px",
-
-      "8xl": "2560px",
     },
   },
   plugins: [],
